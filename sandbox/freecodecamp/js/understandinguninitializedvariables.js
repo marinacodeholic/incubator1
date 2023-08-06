882fa636a2f3922b04f6a540c9d9c62ed76f79a4
marinacodeholic <marina.codeholic@gmail.com>
2023-08-06 16:17:54 +0900

    understandinguninitializedvariables.js

---
+++ sandbox/freecodecamp/js/understandinguninitializedvariables.js
@@ -0,0 +1,14 @@
+//Initialize the three variables a, b, and c with 5, 10, and "I am a"
+// respectively so that they will not be undefined.
+// THEY'RE NEW VALUES. NOW IT'S a = 6; b = 15; c = "I am a String!"
+
+
+let a = 5;
+let b = 10;
+let c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+
