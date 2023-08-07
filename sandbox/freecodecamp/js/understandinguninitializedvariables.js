01919f95da5535533871b879ae1562cd2733893d
marinacodeholic <marina.codeholic@gmail.com>
2023-08-07 14:40:53 +0900

    understandinguninitializedvariables.js

--- sandbox/freecodecamp/js/understandinguninitializedvariables.js
+++ sandbox/freecodecamp/js/understandinguninitializedvariables.js
@@ -3,12 +3,11 @@
 // THEY'RE NEW VALUES. NOW IT'S a = 6; b = 15; c = "I am a String!"
 
 
-let a = 5;
-let b = 10;
-let c = "I am a";
+var a = 5;
+var b = 10;
+var c = "I am a";
 
 a = a + 1;
 b = b + 5;
 c = c + " String!";
-
 
