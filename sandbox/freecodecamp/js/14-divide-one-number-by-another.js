74ae69143312469d1f1a05cfd0f14c9b59ae6533
marinacodeholic <marina.codeholic@gmail.com>
2023-08-10 15:47:25 +0900

    14-divide-one-number-by-another.js

---
+++ sandbox/freecodecamp/js/14-divide-one-number-by-another.js
@@ -0,0 +1,9 @@
+//Divide One Number by Another with JavaScript
+//Change the 0 so that the quotient is equal to 2.
+//const quotient = 66 / 0;
+
+
+
+
+const quotient = 66 / 33;
+console.log(quotient)
