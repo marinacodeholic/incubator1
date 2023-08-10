54ec94435377e141053ab430dba26718cce09a6c
marinacodeholic <marina.codeholic@gmail.com>
2023-08-10 16:18:48 +0900

    15-increment-a-number.js

---
+++ sandbox/freecodecamp/js/15-increment-a-number.js
@@ -0,0 +1,9 @@
+//Change the code to use the`++ operator on`myVar`.
+//let myVar = 87;
+//
+// // Only change code below this line
+// myVar = myVar + 1;
+
+let myVar = 87;
+myVar++;
+console.log(myVar);
