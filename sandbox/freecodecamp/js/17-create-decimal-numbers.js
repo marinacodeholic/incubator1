d587558f8028d587f6c47cbe6116f22f9e6d7b63
marinacodeholic <marina.codeholic@gmail.com>
2023-08-11 18:21:14 +0900

    17-create-decimal-numbers.js

---
+++ sandbox/freecodecamp/js/17-create-decimal-numbers.js
@@ -0,0 +1,4 @@
+//Create a variable myDecimal and give it a decimal value with a fractional part (e.g. 5.7).
+
+const myDecimal = 4.96465415313513541313654341; //15numbers after float
+console.log(myDecimal);
