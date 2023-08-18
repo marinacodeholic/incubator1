fcbd494f054826e8cc330cf8922eabf926eb01fb
marinacodeholic <marina.codeholic@gmail.com>
2023-08-19 08:26:43 +0900

    24-compound-assignment-with-augment-division.js

---
+++ sandbox/freecodecamp/js/24-compound-assignment-with-augment-division.js
@@ -0,0 +1,17 @@
+//Convert the assignments for a, b, and c to use the /= operator.
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+/*
+a = 48 / 12;
+b = 104 / 4;
+c = 33 / 11;
+*/
+
+a /= 12;
+b /= 4;
+c /= 11;
+
+console.log(a, b, c);
