c8ebc005e0c1f600e6440ca609e3e9ce4a921c17
marinacodeholic <marina.codeholic@gmail.com>
2023-08-21 06:47:32 +0900

    samp.js

---
+++ practice/samp.js
@@ -0,0 +1,32 @@
+const http = require('http');
+
+let requestCount = 0;
+
+const server = http.createServer((request, response) => {
+    requestCount++;
+
+    switch (request.url) {
+        case '/':
+        case '/students':
+            response.write('courses ');
+        break;
+        case 'courses':
+            response.write('FRONT BACK ');
+            break;
+        default:
+            response.write('404 ')
+    }
+    response.write('IT-KAMASUTRA: ' + requestCount);
+    response.end();
+})
+
+server.listen(2023)
+
+/*
+let counter = 0;
+
+setInterval(() => {
+    counter++
+    console.log(counter);
+
+}, 1000);*/
