33436fb98e34821e001f20c44182b7c4a1c5b858
marinacodeholic <marina.codeholic@gmail.com>
2023-08-24 00:24:27 +0900

    samp.js

--- practice/samp.js
+++ practice/samp.js
@@ -1,17 +1,17 @@
 const http = require('http');
 
+
 let requestCount = 0;
 
 const server = http.createServer((request, response) => {
     requestCount++;
-
 
     switch (request.url) {
         case '/':
         case '/students':
             response.write('courses ');
         break;
-        case 'courses':
+        case '/courses':
             response.write('FRONT BACK ');
             break;
         default:
@@ -21,7 +21,7 @@ const server = http.createServer((request, response) => {
     response.end();
 })
 
-server.listen(2023)
+server.listen(2023);
 
 /*
 let counter = 0;
